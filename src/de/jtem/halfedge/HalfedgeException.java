f2b7bb87df0903afbb6fbd3495d006577670f05e
Stefan Sechelmann <sechel@math.tu-berlin.de>
2012-07-27 17:08:56 +0000

    a half edge exception
    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@1311 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

---
+++ src/de/jtem/halfedge/HalfedgeException.java
@@ -0,0 +1,23 @@
+package de.jtem.halfedge;
+
+public class HalfedgeException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public HalfedgeException() {
+		super();
+	}
+
+	public HalfedgeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public HalfedgeException(String message) {
+		super(message);
+	}
+
+	public HalfedgeException(Throwable cause) {
+		super(cause);
+	}
+	
+}
