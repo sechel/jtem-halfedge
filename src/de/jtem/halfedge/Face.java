8fc11899490f5f94d4ac1548fb04b0b4ade64a85
Charles Gunn <gunn@math.tu-berlin.de>
2013-05-17 09:30:43 +0000

    git-svn-id: https://svn.math.tu-berlin.de/svn/jtem@1572 f5b180c5-49ee-4939-b20e-b6ed35f0f7b7

--- src/de/jtem/halfedge/Face.java
+++ src/de/jtem/halfedge/Face.java
@@ -82,8 +82,10 @@ public abstract class Face <V extends Vertex<V, E, F>,
 		return boundaryEdge;
 	}
 
-	public final void setBoundaryEdge(E e) {
+	final public void setBoundaryEdge(E e) {
 		checkHalfEdgeDataStructure(e);
+		if (!hds.getEdges().contains(e))
+			throw new IllegalStateException("Not an edge of this face");
 		this.boundaryEdge = e;
 	};
 	
